8d4f92035d0cc06907f5aaeda1e12462972be681
LothMoria <chucknoris3@gmail.com>
2018-05-30 14:07:51 -0400

    Did some car stuffs

---
+++ js/script.js
@@ -0,0 +1,26 @@
+var car = {
+	make: 'Saturn',
+	type: 'Ion',
+	color: 'Silver',
+	isOn: false,
+	wheelNum: 4,
+	seats: ['s1', 
+			's2', 
+			's3', 
+			's4', 
+			's5'],
+	turnOn: function () {
+		this.isOn = true;
+	},
+	fly: function () {
+		alert('fly');
+	},
+	switchCar: function () {
+		if(this.isOn == true) {
+			this.isON = false;
+		}else{
+			this.isOn = true;
+		}
+	}
+};	
+console.log('Hello there friends!');
