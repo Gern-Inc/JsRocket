c79ff4d87d2f3b56c69542f3819e180064fde854
LothMoria <chucknoris3@gmail.com>
2018-06-14 20:02:33 -0400

    More Rocket Stuff

--- js/rocket.js
+++ js/rocket.js
@@ -1,3 +1,29 @@
+var timer = null; 
+var count = 10;
 var changeState = function (state) {
 	document.body.className = 'bState' + state;
+	clearInterval(timer);
+
+	if(state==2){
+		timer = setInterval(function()
+		{
+			document.getElementById('countDown').innerHTML = count;
+			count = count - 1;
+			if(count <= -1){
+				changeState(3);
+			};
+		},500);
+	}
+	else if(state == 3){
+		var success = setTimeout(funtion()
+		{
+			var randNum = Math.round(Math.random()*10);
+			if(randNum > 5)
+			{
+				changeState(4);
+			}else{
+				changeState(5);
+			}
+		},2000);
+	};
 }
